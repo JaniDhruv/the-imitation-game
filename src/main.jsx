2e307780bb0bdfae2800d20d8f6ed4c0e4cb3800
Dhruv <dhruvdjani29@gmail.com>
2026-06-11 21:47:42 +0530

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import App from './App.jsx'
 import './index.css'
 import { GameProvider } from './context/GameContext.jsx'
 
+import { Analytics } from '@vercel/analytics/react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <GameProvider>
         <App />
+        <Analytics />
       </GameProvider>
     </BrowserRouter>
   </React.StrictMode>,
